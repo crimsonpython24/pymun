8fd86f934d17b766f73f8627d591d01dbcd0dc62
Warren Wang <crimsonpython24@gmail.com>
2020-01-21 18:49:35 +0800

    adding meta keywords

--- pymun/utils/models.py
+++ pymun/utils/models.py
@@ -124,36 +124,34 @@ class MetaTagsMixin(models.Model):
     class Meta:
         abstract = True
 
-    meta_keywords = models.CharField(_("Keywords"), max_length=255, blank=True, help_text=_("Separate keywords by comma."))
-    meta_description = models.CharField(_("Description"), max_length=255, blank=True)
-    meta_author = models.CharField(_("Author"), max_length=255, blank=True)
-    meta_copyright = models.CharField(_("Copyright"), max_length=255, blank=True)
+    meta_keywords = models.CharField(_("Keywords"), max_length=255,
+                                     blank=True, help_text=_("Separate keywords by comma."))
+    meta_username = models.CharField(_("Username"), max_length=255, blank=True)
+    meta_email = models.CharField(_("Email"), max_length=255, blank=True)
+    meta_name = models.CharField(_("Name"), max_length=255, blank=True)
 
-    @staticmethod
-    def get_meta(self, name, content):
+    def get_meta(self, username, email):
         tag = ""
-        if name and content:
-            tag = loader.render_to_string('utils/meta.html', {'name': name, 'content': content})
+        if username and email:
+            tag = loader.render_to_string('utils/meta.html', {'username': username, 'email': email})
         return mark_safe(tag)
 
     def get_meta_keywords(self):
         return self.get_meta('keywords', self.meta_keywords)
 
-    def get_meta_description(self):
-        return self.get_meta('description', self.meta_description)
+    def get_meta_username(self):
+        return self.get_meta('username', self.meta_username)
 
-    def get_meta_author(self):
-        return self.get_meta('author', self.meta_author)
+    def get_meta_email(self):
+        return self.get_meta('email', self.meta_email)
 
-    def get_meta_copyright(self):
-        return self.get_meta('copyright', self.meta_copyright)
+    def get_meta_name(self):
+        return self.get_meta('name', self.self.meta_name)
 
     def get_meta_tags(self):
-        return mark_safe("\n".join((
-            self.get_meta_keywords(),
-            self.get_meta_description(),
-            self.get_meta_author(),
-            self.get_meta_copyright(),
+        return mark_safe(
+            "\n".join((self.get_meta_keywords(), self.get_meta_username(),
+                       self.get_meta_email(), self.get_meta_name(),
         )))
 
 
